4381b95835c10dbad8f9435d39f6a2a2f3c6a26a
Danilo Hernandez <eldh.dev@gmail.com>
2025-08-05 01:02:31 -0600

    update

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        $users = User::paginate();
+        $users = User::simplePaginate($request->per_page);
 
         return UserResource::collection($users);
     }
